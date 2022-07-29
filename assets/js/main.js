dcd7547a626bd2e8171471e2960f8fe774fa06df
Sakil Anwar <sakilanwar60@gmail.com>
2022-07-29 09:11:04 +0600

    complete responsive website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var swiper = new Swiper(".testimonial", {
-        slidesPerView: 2,
-        spaceBetween: 30,
+    // navbar
+    const menuBtn = document.querySelector('.menuBtn');
+    const mobileMenu = document.querySelector('.mobileMenu');
+    const closeMenu = document.querySelector('.closeMenu');
+    menuBtn.addEventListener('click', function () {
+        mobileMenu.classList.add('showMenu');
+        mobileMenu.classList.remove('hideMenu');
+    })
+    closeMenu.addEventListener('click', function () {
+        mobileMenu.classList.add('hideMenu');
+        mobileMenu.classList.remove('showMenu');
+    })
+    
+    var heroSlider = new Swiper(".heroSlider", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: ".hero .swiper-button-prev",
+            prevEl: ".hero .swiper-button-next",
+        },
+    });
+    
+    var testimonial = new Swiper(".testimonial", {
+        slidesPerView: 1,
         loop: true,
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+         breakpoints: {
+             '1056': {
+                 slidesPerView: 2,
+                 spaceBetween: 30
+             }
+         }
     });
+
 })
